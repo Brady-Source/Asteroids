acc2c83c3ee8620cd20d38124e00c7848271a144
Brady-Source <finnellbrady@gmail.com>
2026-07-19 12:22:18 -0400

    Completed the base of game-loop

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -22,6 +25,8 @@ def main():
         screen.fill("black") # Sets the BG to black
 
         pygame.display.flip() #CALL THIS LAST - Refreshes the screen
+
+        dt = clock.tick(60) /1000
 
 if __name__ == "__main__":
     main()
