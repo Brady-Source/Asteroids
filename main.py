d8451e25c3e92ed71552117fdceb80bfbd68ba25
Brady-Source <finnellbrady@gmail.com>
2026-07-19 11:50:33 -0400

    Added BG and quit functionality

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
 
 def main():
+    pygame.init()
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill("black") # Sets the BG to black
+
+        pygame.display.flip() #CALL THIS LAST - Refreshes the screen
 
 if __name__ == "__main__":
     main()
